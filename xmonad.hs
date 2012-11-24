7f65864fae9cebd8ab874c0bfeee9eb8bb24db34
James <jamestloyd@gmail.com>
2012-11-24 16:11:18 -0500

    modified:   xmonad.hs

--- xmonad.hs
+++ xmonad.hs
@@ -6,19 +6,25 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.UrgencyHook
 import XMonad.Hooks.InsertPosition
-
-
-main =  xmonad =<< xmobar  myConfig
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Layout.NoBorders
+import XMonad.Hooks.ManageHelpers(doFullFloat,isFullscreen)
+main = xmonad =<< xmobar  myConfig
 myConfig = defaultConfig     
         { modMask = mod4Mask
-         , terminal = "terminator"
+         ,terminal = "terminator"
 	 , workspaces = myWorkspaces
 	, startupHook =  myStartup
+	, handleEventHook = fullscreenEventHook
+	, layoutHook = smartBorders $ layoutHook defaultConfig
+	, manageHook = composeOne [
+	      isFullscreen -?> doFullFloat
+          ]
        }
-
---Workspaces
-myWorkspaces = ["1:Home" , "2:Web" , "3:Media" , "4:Development" , "5:Stuff" , "6:Terminals", "7:Booty" , "8:lolcats", "9:minecraft" ]
+--Workspaces for my computer.
+myWorkspaces = ["1:Home" , "2:Web" , "3:Media" , "4:Development" , "5:Stuff" , "6:Terminals", "7:File Manager" , "8:IM", "9:Moar" ]
 
 --Startup
 myStartup = do
     spawn "bash $HOME/.fehbg"
+
